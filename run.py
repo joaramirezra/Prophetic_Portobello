681055dd9f2982ddaa6554520fd943b031199f3c
joaramirezra <joaramirezra@unal.edu.co>
2023-08-23 21:18:32 -0500

    version 0.6.0 create simple landing page , it should have a better image selection

--- run.py
+++ run.py
@@ -1,4 +1,14 @@
-from flask import Flask
-app = Flask(__name__)
+from flask import Flask, render_template
+
+
+app = Flask(__name__, template_folder='app/templates', static_folder='app/static')
+
+
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
 if __name__ == '__main__':
     app.run(debug=True)
+
